0a4305affe084139587b5f4c0f62f2a521ec7971
Ariel Young <ariyoung@pm.me>
2024-02-06 18:38:11 -0800

    Added CVPR dataset with git-lfs, added prepare_data.sh, updated gitignore

---
+++ data/prepare_data.sh
@@ -0,0 +1,8 @@
+# This script will take the raw data as downloaded from kaggle, etc. and
+# organize it in the way our code expects it to be organized. The raw data archives should be
+# tracked with git-lfs. As you add data, please update this script to move it into place.
+set -e
+
+# MIT CVPR 2019
+cd cvpr_raw
+unzip ../archives/mit_cvpr_2019.zip
