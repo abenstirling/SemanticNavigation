bcab716936a8d3d100020abdeedc566dc865a736
Ariel Young <ariyoung@pm.me>
2024-02-11 19:44:08 -0800

    Update add depth notebook, prepare data script

--- data/prepare_data.sh
+++ data/prepare_data.sh
@@ -3,6 +3,9 @@
 # tracked with git-lfs. As you add data, please update this script to move it into place.
 set -e
 
+# cd to the dir of this script
+cd "$(dirname "$0")"
+
 # MIT CVPR 2019
 cd cvpr_raw
 unzip ../archives/mit_cvpr_2019.zip
